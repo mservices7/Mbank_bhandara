a22a2ef7f20ef1867d7387539f9b7e29712b3932
Rohit-Parate <rinku.parate@gmail.com>
2016-03-17 16:36:37 +0530

    17-3-2016
    test

--- mBankUI_V_1.1/pages/create/create_user/create_user.ctrl.js
+++ mBankUI_V_1.1/pages/create/create_user/create_user.ctrl.js
@@ -40,14 +40,18 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
             var trx1 = response; var user1 = trx1.value; var count = user1.length;
             $scope.maxLoginUsers= user1[0].login_id;
             $scope.maxLoginExterUser= user1[0].external_login_id;
-          
+          // alert('userTable maxLoginExterUser' + $scope.maxLoginExterUser);
         })
 
         $http.get(linkglobal + '/agents?$orderby=external_agent_id desc').success(function (response) {
             
 
-            var amt = 0; var trx1 = response; var user1 = trx1.value;
-             $scope.maxAgentExterId=Math.max(user1[0].external_agent_id); 
+            var amt = 0;
+            var trx1 = response;
+            var user1 = trx1.value;
+            $scope.maxAgentExterId = Math.max(user1[0].external_agent_id);
+           //alert('AgentTable maxAgentExterId' + $scope.maxAgentExterId);
+
         })
        
     }
@@ -81,7 +85,7 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
     var cpassword = String(this.acpass);
     var plus2 = $scope.maxAgentExterId;
     
-    var plusOne2 = parseInt(plus2) + 2;
+    var plusOne2 = parseInt(plus2) + 1;
     var bank_id =imageIDData;
     var branch_id = this.agentBranchID;
     var role_id =1;
@@ -92,6 +96,7 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
     var plusOne1 = parseInt(plus1)+1;
    
     var external_login_id = String(plusOne);
+   // alert('external_login_id for user' + external_login_id);
     var login_id = plusOne1;
     var role_id;
     var status=1;
@@ -99,7 +104,10 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
     var sync_dt = String($scope.ModifiedDate);
     var bank_sync_dt = String($scope.ModifiedDate);
     var external_agent_id = plusOne2;
+    //alert('external_agent_id for agent + ' + external_agent_id);
 
+   // alert('login_id' + login_id);
+    //alert(password);
         var request = $http({
             method: "post",
             url: linkglobal + "/user_details",
@@ -112,8 +120,8 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
                 status:1,
                 is_sync : true,
                 sync_dt : String($scope.ModifiedDate),
-                bank_sync_dt : String($scope.ModifiedDate)
-
+                bank_sync_dt : String($scope.ModifiedDate),
+                bank_id:$scope.imgIdDdURL
             },
   
         headers: { 'Content-Type': 'application/json' },
@@ -127,7 +135,8 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
             url: linkglobal + "/agents",
             crossDomain: true,
             data: {
-                external_agent_id: String(login_id),
+               // external_agent_id: String(login_id),
+                external_agent_id: String(external_agent_id),
                 agent_name: String(agent_name),
                 agent_local_add: String(agent_local_add),
                 agent_perm_add: String(agent_perm_add),
@@ -147,7 +156,7 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
 
         }).success(function () {
 
-            $http.get(linkglobal + '/agents?$filter=bank_id eq ' + imageIDData + ' and external_agent_id+eq+' + "'" + login_id + "'").success(function (response) {
+            $http.get(linkglobal + '/agents?$filter=bank_id eq ' + imageIDData + ' and external_agent_id+eq+' + "'" + external_agent_id + "'").success(function (response) {
                 var amt = 0;
                 var agent = response;
                 var agents = agent.value;
@@ -159,7 +168,8 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
                     url: linkglobal + "/agents(" + agent_id + ")",
                     crossDomain: true,
                     data: {
-                        external_agent_id: String(login_id),
+                     //   external_agent_id: String(login_id),
+                        external_agent_id: String(external_agent_id),
                         agent_name: String(agent_name),
                         agent_local_add: String(agent_local_add),
                         agent_perm_add: String(agent_perm_add),
@@ -178,13 +188,13 @@ scotchApp.controller('userController', function ($rootScope, $interval, $timeout
                     headers: { 'Content-Type': 'application/json' },
 
                 })
-            });
+     });
 
 
 
             $scope.clearData();
 
-            alert(agent_name + 'Agent Create');
+            alert(agent_name + ' Agent Create');
         })
 
 
